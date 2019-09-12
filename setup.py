eea81781c958f6b53f61702cf1a6a8eba77e6161
thomaswilliamsastro <thomas.williams@astro.cf.ac.uk>
2019-09-12 21:57:23 +0100

    Included setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='data_buttons',
+    version='0.1',
+    packages=['data_buttons'],
+    url='http://data-buttons.readthedocs.io',
+    license='GPL-3.0',
+    author='Thomas Williams',
+    author_email='thomas.g.williams94@gmail.com',
+    description='Useful Catalogue Query/Mosaicking Tools',
+    install_requires=['astropy','numpy','astroquery','MontagePy']
+)
